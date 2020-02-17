75213de03566f6a95b80db231b019e2ce1e67627
Marcele Godoy <marcelegodoy@gmail.com>
2020-02-17 15:20:30 -0300

    Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,10 +9,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-      if @question.include?("?")
-        @answer = "Silly question, get dressed and go to work!"
-      elsif @question == "I am going to work"
-        @answer = "Great!"
+      if @question.include?('?')
+        @answer = 'Silly question, get dressed and go to work!'
+      elsif @question == 'I am going to work'
+        @answer = 'Great!'
       else
         @answer = "I don't care, get dressed and go to work!"
       end
